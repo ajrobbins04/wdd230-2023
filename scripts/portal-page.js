b656927cead48bccdd89941ad7322776e31b5c33
ajrobbins04 <ajrobbins04@gmail.com>
2023-01-08 14:30:54 -0800

    portal page almost done

--- scripts/portal-page.js
+++ scripts/portal-page.js
@@ -2,3 +2,6 @@ const date = new Date();
  
 // Display date last modified in footer.
 document.getElementById('last-updated').innerHTML = `Last Updated: ${document.lastModified}`;
+
+let year = date.getFullYear();
+document.querySelector('#copyright-year').textContent = ` ${year}`;  
